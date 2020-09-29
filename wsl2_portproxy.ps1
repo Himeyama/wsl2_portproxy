e88cfa2e8bdf36ca88db6ad617cc380e2a291a05
Murata Mitsuharu <hikari.photon+mygit@gmail.com>
2020-09-29 11:54:11 +0900

    with bom

--- wsl2_portproxy.ps1
+++ wsl2_portproxy.ps1
@@ -1,4 +1,4 @@
-if (-not(([Security.Principal.WindowsPrincipal] `
+﻿if (-not(([Security.Principal.WindowsPrincipal] `
     [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
     [Security.Principal.WindowsBuiltInRole] "Administrator"`
     ))) {
